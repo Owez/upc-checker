40925595eebe06108b4f9a6717a540d716f72904
scOwez <owez@scalist.net>
2019-07-21 00:55:07 +0100

    Removed mod Reformatted Cargo.toml for proper author name and no comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,60 @@
-mod upc_code {
-    use std::io;
+use std::io;
+
+#[allow(dead_code)]
+pub struct UPCCode {
+    pub code: Vec<i8>,
+    pub check_digit: i8,
+}
+
+impl UPCCode {
+    #[allow(dead_code)]
+    fn validate_nums(&self) -> bool {
+        for code in &self.code {
+            if !is_1_digit(*code) {
+                return false;
+            }
+        }
+
+        is_1_digit(self.check_digit)
+    }
 
     #[allow(dead_code)]
-    pub struct UPCCode {
-        pub code: Vec<i8>,
-        pub check_digit: i8,
-    }
+    fn add_even_odd_total(&self) -> (i8, i8) {
+        let mut result: (i8, i8) = (0, 0);
 
-    impl UPCCode {
-        #[allow(dead_code)]
-        fn validate_nums(&self) -> bool {
-            for code in &self.code {
-                if !is_1_digit(*code) {
-                    return false;
-                }
+        for code in &self.code {
+            if code % 2 == 0 {
+                result.0 += code;
+            } else {
+                result.1 += code;
             }
-
-            is_1_digit(self.check_digit)
         }
 
-        #[allow(dead_code)]
-        fn add_even_odd_total(&self) -> (i8, i8) {
-            let mut result: (i8, i8) = (0, 0);
-
-            for code in &self.code {
-                if code % 2 == 0 {
-                    result.0 += code;
-                } else {
-                    result.1 += code;
-                }
-            }
-
-            result
-        }
-
-        #[allow(dead_code)]
-        pub fn check_code(&self) -> Result<bool, io::Error> {
-            if !&self.validate_nums() {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "A number used isn\'t 1 digit!",
-                ));
-            }
-
-            let mut total: i8 = 0;
-            let (even_nums, odd_nums) = &self.add_even_odd_total();
-
-            total += ((odd_nums * 3) + even_nums) % 10;
-
-            if (total == 0 && self.check_digit == 0) || (10 - total == self.check_digit) {
-                return Ok(true);
-            }
-
-            Ok(false)
-        }
+        result
     }
 
-    fn is_1_digit(num: i8) -> bool {
-        !(num < 0 || num > 9)
+    #[allow(dead_code)]
+    pub fn check_code(&self) -> Result<bool, io::Error> {
+        if !&self.validate_nums() {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "A number used isn\'t 1 digit!",
+            ));
+        }
+
+        let mut total: i8 = 0;
+        let (even_nums, odd_nums) = &self.add_even_odd_total();
+
+        total += ((odd_nums * 3) + even_nums) % 10;
+
+        if (total == 0 && self.check_digit == 0) || (10 - total == self.check_digit) {
+            return Ok(true);
+        }
+
+        Ok(false)
     }
+}
+
+fn is_1_digit(num: i8) -> bool {
+    !(num < 0 || num > 9)
 }
