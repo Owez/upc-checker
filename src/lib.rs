d71bb5de1e43d5a885411f9de7b1d411e544f23f
scOwez <owez@scalist.net>
2019-07-21 18:26:03 +0100

    Better and slashed down docs

--- src/lib.rs
+++ src/lib.rs
@@ -77,7 +77,7 @@ mod tests {
 ///
 /// let my_upc_code = UPCCode {
 ///     code: my_code_vector,
-///     check_digit: my_check_digit
+///     check_digit: my_check_digit,
 /// };
 /// 
 /// println!("Is `code` valid?: {}", my_upc_code.check_code().unwrap());
@@ -91,21 +91,6 @@ pub struct UPCCode {
 impl UPCCode {
     /// Validates that all data in the `UPCCode` is 1-char in len (0-9)
     /// instead of the -255 to 255 of an i8
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// let is_valid_nums = &self.validate_nums();
-    ///
-    /// if !&self.validate_nums() {
-    ///     return Err(io::Error::new(
-    ///         io::ErrorKind::Other,
-    ///         "A number used isn\'t 1 digit!",
-    ///     ));
-    /// }
-    ///
-    /// // Numbers are valid
-    /// ```
     fn validate_nums(&self) -> bool {
         for code in self.code.iter() {
             if !is_1_digit(*code) {
@@ -118,13 +103,6 @@ impl UPCCode {
 
     /// Adds odd and even numbers (using %) to a 2-len
     /// u8 tuple `(even, odd)` respectivly
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// let (even_nums, odd_nums) = &self.add_even_odd_total();
-    /// println!("Even: {0}, Odd: {1}", even_nums, odd_nums);
-    /// ```
     fn add_even_odd_total(&self) -> (u8, u8) {
         let mut result: (u8, u8) = (0, 0);
 
@@ -148,7 +126,13 @@ impl UPCCode {
     /// # Examples
     ///
     /// ```rust
-    /// let my_struct = UPCCode {code: vec![3, 5, 7, 4], check_digit: 3};
+    /// extern crate upc_check;
+    /// 
+    /// let my_struct = upc_check::UPCCode {
+    ///     code: vec![3, 5, 7, 4],
+    ///     check_digit: 3,
+    /// };
+    /// 
     /// println!("Result: {}", my_struct.check_code().unwrap());
     /// ```
     pub fn check_code(&self) -> Result<bool, Error> {
@@ -173,19 +157,6 @@ impl UPCCode {
 }
 
 /// Checks if a given i8 is 1 character wide (0-9)
-///
-/// # Examples
-///
-/// ```rust
-/// let digit_to_check: i8 = 9;
-/// let should_be_invalid: i8 = 102;
-///
-/// println!(
-///     "Is `digit_to_check` valid?: {}. Is `should_be_invalid` valid?: {}",
-///     is_1_digit(digit_to_check),
-///     is_1_digit(should_be_invalid)
-/// )
-/// ```
 fn is_1_digit(num: i8) -> bool {
     !(num < 0 || num > 9)
 }
