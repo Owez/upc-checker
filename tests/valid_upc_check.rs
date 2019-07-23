3fa743c49d640e1465f62b28584b51062660621d
scOwez <owez@scalist.net>
2019-07-23 23:29:30 +0100

    Added upc e check Added docstrings onto tests

--- tests/valid_upc_check.rs
+++ tests/valid_upc_check.rs
@@ -1,8 +1,25 @@
 extern crate upc_checker;
 
+/// Checks if check_upc is returning the right values for
+/// [UPC-A](https://en.wikipedia.org/wiki/Universal_Product_Code#Encoding)
 #[test]
 fn valid_upc_a_check() {
     let my_upc = upc_checker::UPCStandard::UPCA([9,9,9,9,9,9,9,9,9,9,9,9]);
+    let my_check_code: i8 = 7;
+
+    let my_upc_struct = upc_checker::UPC {
+        upc: my_upc,
+        check_digit: my_check_code,
+    };
+
+    assert_eq!(Ok(true), my_upc_struct.check_upc());
+}
+
+/// Checks if check_upc is returning the right values for
+/// [UPC-E](https://en.wikipedia.org/wiki/Universal_Product_Code#UPC-E)
+#[test]
+fn valid_upc_e_check() {
+    let my_upc = upc_checker::UPCStandard::UPCE([9,9,9,9,9,9,9,9]);
     let my_check_code: i8 = 7;
 
     let my_upc_struct = upc_checker::UPC {
